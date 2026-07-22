d5733b6d62ae497f02912f8f9b73a316068641b0
pratyaksh2012 <sundar.mdic@gmail.com>
2026-07-22 21:27:42 +0530

    Create install.js

---
+++ js/install.js
@@ -0,0 +1,27 @@
+let deferredPrompt;
+
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+
+    e.preventDefault();
+
+    deferredPrompt = e;
+
+    installBtn.style.display = "block";
+
+});
+
+installBtn.addEventListener("click", async () => {
+
+    if (!deferredPrompt) return;
+
+    deferredPrompt.prompt();
+
+    await deferredPrompt.userChoice;
+
+    deferredPrompt = null;
+
+    installBtn.style.display = "none";
+
+});
