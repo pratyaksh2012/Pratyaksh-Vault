c78b49ca30c4468219ff5e412f28109f9c4cde6a
pratyaksh2012 <sundar.mdic@gmail.com>
2026-07-22 15:13:31 +0530

    Create chat.js

---
+++ js/chat.js
@@ -0,0 +1,46 @@
+import {
+getFirestore,
+collection,
+getDocs
+}
+from
+"https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import {app}
+from "./firebase.js";
+
+const db=getFirestore(app);
+
+const search=document.getElementById("searchBox");
+
+const results=document.getElementById("searchResults");
+
+search.addEventListener("keyup",async()=>{
+
+const snap=await getDocs(collection(db,"users"));
+
+results.innerHTML="";
+
+snap.forEach(doc=>{
+
+const user=doc.data();
+
+if(user.name.toLowerCase().includes(search.value.toLowerCase())){
+
+results.innerHTML+=`
+
+<div class="user">
+
+${user.name}
+
+<button>Add</button>
+
+</div>
+
+`;
+
+}
+
+});
+
+});
