d11c805f9e36513f0de31dc45bc8baaeaaf87a9e
pratyaksh2012 <sundar.mdic@gmail.com>
2026-07-22 21:50:28 +0530

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,36 +1,26 @@
 const CACHE_NAME = "pratyaksh-vault-v1";
 
-const urls = [
-"/",
-"/index.html",
-"/dashboard.html",
-"/css/style.css",
-"/css/dashboard.css",
-"/js/install.js",
-"/icons/icon-192.png",
-"/icons/icon-512.png"
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./dashboard.html",
+    "./css/style.css",
+    "./css/dashboard.css",
+    "./js/install.js",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
-self.addEventListener("install",e=>{
-
-e.waitUntil(
-
-caches.open(CACHE_NAME)
-
-.then(cache=>cache.addAll(urls))
-
-);
-
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
-self.addEventListener("fetch",e=>{
-
-e.respondWith(
-
-caches.match(e.request)
-
-.then(response=>response || fetch(e.request))
-
-);
-
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
