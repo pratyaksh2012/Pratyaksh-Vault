5ba1c0db9c9a05c9d92347ee743f3ebe2f527eac
pratyaksh2012 <sundar.mdic@gmail.com>
2026-07-22 15:27:06 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,31 +2,34 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
 import {
-    getAuth
+  getAuth
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 import {
-    getFirestore
+  getFirestore
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+import {
+  getStorage
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
+
+// Firebase Configuration
 const firebaseConfig = {
-
-    apiKey: "AIzaSyBNxE4G1N0v6ronOt88kmrNKLDJKADSXtE",
-
-    authDomain: "cloudshare-pro-e3dc1.firebaseapp.com",
-
-    projectId: "cloudshare-pro-e3dc1",
-
-    storageBucket: "cloudshare-pro-e3dc1.firebasestorage.app",
-
-    messagingSenderId: "814589791555",
-
-    appId: "1:814589791555:web:37489444d27d2409481b06"
-
+  apiKey: "AIzaSyBNxE4G1N0v6ronOt88kmrNKLDJKADSXtE",
+  authDomain: "cloudshare-pro-e3dc1.firebaseapp.com",
+  projectId: "cloudshare-pro-e3dc1",
+  storageBucket: "cloudshare-pro-e3dc1.firebasestorage.app",
+  messagingSenderId: "814589791555",
+  appId: "1:814589791555:web:37489444d27d2409481b06"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+// Services
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-export const db = getFirestore(app);
+// Export Services
+export { app, auth, db, storage };
